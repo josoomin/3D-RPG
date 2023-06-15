7c15a3f2f487d10fa246c499fad45830e12d6576
josoomin <99530921+josoomin@users.noreply.github.com>
2023-06-15 14:12:25 +0900

    3D

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dreamingcat
+{
+    public class Player : MonoBehaviour
+    {
+        public BoxCollider _attackCol;
+
+        float _speed = 10.0f;
+        float _rotateSpeed = 100.0f; // 회전 속도
+        float _jumppower = 5.0f;
+
+        bool _Plane;
+
+        Rigidbody _myRigidbody;
+
+        Animator _myAni;
+
+        void Start()
+        {
+            _myRigidbody = GetComponent<Rigidbody>();
+            _myAni = GetComponent<Animator>();
+            _attackCol.enabled = false;
+        }
+
+        void Update()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (h != 0 || v != 0)
+            {
+                Move(h, v);
+                _myAni.SetBool("Walk", true);
+            }
+            else
+            {
+                _myAni.SetBool("Walk", false);
+                _myRigidbody.angularVelocity = new Vector3(0, 0, 0);
+            }
+
+
+            if (Input.GetKey(KeyCode.Space) && _Plane)
+            {
+                Jump();
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+            }
+        }
+        public void Move(float h, float v)
+        {
+            Vector3 dir = new Vector3(h, 0, v); // new Vector3(h, 0, v)가 자주 쓰이게 되었으므로 dir이라는 변수에 넣고 향후 편하게 사용할 수 있게 함
+
+            transform.Translate(new Vector3(0, 0, v * _speed) * Time.deltaTime);
+            transform.Rotate(new Vector3(0, h * _rotateSpeed, 0) * Time.deltaTime);
+        }
+
+        public void Jump()
+        {
+            _myRigidbody.AddForce(Vector3.up * _jumppower, ForceMode.Impulse);
+            _Plane = false;
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Plane"))
+            {
+                _Plane = true;
+            }
+        }
+
+        public void Attack()
+        {
+            _attackCol.enabled = true;
+            _myAni.SetTrigger("Attack");
+        }
+
+        public void NotAttack()
+        {
+            _attackCol.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace josoomin
+{
+    public class RotateCamera : MonoBehaviour
+    {
+        private float xRotate, yRotate, xRotateMove, yRotateMove;
+        public float rotateSpeed = 500.0f;
+        int _xMaxValue = -10;
+
+        void Update()
+        {
+            xRotateMove = -Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed;
+            yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+            yRotate = transform.eulerAngles.y + yRotateMove;
+            //xRotate = transform.eulerAngles.x + xRotateMove; 
+            xRotate = xRotate + xRotateMove;
+
+            xRotate = Mathf.Clamp(xRotate, -90, 90); // 위, 아래 고정
+
+           
+
+            if (xRotate >= _xMaxValue)
+            {
+                transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(_xMaxValue, yRotate, 0);
+            }
+        }
+    }
+}
