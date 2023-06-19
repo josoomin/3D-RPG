2664f4f75ef626d7181cfdcd8781c742821504db
josoomin <99530921+josoomin@users.noreply.github.com>
2023-06-19 17:23:35 +0900

    ani

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace josoomin
+{
+    public class Monster : MonoBehaviour
+    {
+        
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,28 +7,36 @@ namespace dreamingcat
     public class Player : MonoBehaviour
     {
         public BoxCollider _attackCol;
+        public BoxCollider _defandCol;
+
+        public GameObject _head;
 
         float _speed = 10.0f;
         float _rotateSpeed = 100.0f; // 회전 속도
         float _jumppower = 5.0f;
 
-        bool _Plane;
+        bool _plane;
+        bool _defand;
 
         Rigidbody _myRigidbody;
+        CapsuleCollider _myCapCol;
 
         Animator _myAni;
 
         void Start()
         {
             _myRigidbody = GetComponent<Rigidbody>();
+            _myCapCol = GetComponent<CapsuleCollider>();
             _myAni = GetComponent<Animator>();
             _attackCol.enabled = false;
+            _defandCol.enabled = false;
         }
 
         void Update()
         {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
+            //_myCapCol.transform.position = _head.transform.position;
 
             if (h != 0 || v != 0)
             {
@@ -41,16 +49,26 @@ namespace dreamingcat
                 _myRigidbody.angularVelocity = new Vector3(0, 0, 0);
             }
 
-
-            if (Input.GetKey(KeyCode.Space) && _Plane)
+            if (Input.GetKey(KeyCode.Space) && _plane)
             {
                 Jump();
             }
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && !_defand)
             {
                 Attack();
             }
+
+            if (Input.GetMouseButton(1))
+            {
+                Defand();
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                NotDefand();
+            }
+
         }
         public void Move(float h, float v)
         {
@@ -62,15 +80,17 @@ namespace dreamingcat
 
         public void Jump()
         {
+            _myAni.SetTrigger("Jump");
             _myRigidbody.AddForce(Vector3.up * _jumppower, ForceMode.Impulse);
-            _Plane = false;
+            _plane = false;
         }
 
         void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.CompareTag("Plane"))
             {
-                _Plane = true;
+                _myCapCol.enabled = true;
+                _plane = true;
             }
         }
 
@@ -83,6 +103,20 @@ namespace dreamingcat
         public void NotAttack()
         {
             _attackCol.enabled = false;
+        }
+
+        public void Defand()
+        {
+            _defand = true;
+            _defandCol.enabled = true;
+            _myAni.SetBool("Defand", true);
+        }
+
+        public void NotDefand()
+        {
+            _defand = false;
+            _defandCol.enabled = false;
+            _myAni.SetBool("Defand", false);
         }
     }
 }
